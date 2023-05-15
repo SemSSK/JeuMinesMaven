383aa90003fcae35c256ceb7ba21487053e4213d
semssk <ssksem015@gmail.com>
2023-05-15 23:03:05 +0100

    Put documentation in dedicated folder

---
+++ images/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"mines","l":"Board"},{"p":"mines","l":"Cell"},{"p":"mines","l":"Field"},{"p":"mines","l":"GameStates"},{"p":"mines","l":"Mines"}];updateSearchResults();
